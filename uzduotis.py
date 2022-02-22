0ad864af0776f448ae11ecbb8745b7e9645d9236
Andrejus <andrejusantoninovas@gmail.com>
2022-02-22 15:59:54 +0200

    Pridėta emailų siuntimo imitavimo logika

--- uzduotis.py
+++ uzduotis.py
@@ -1,11 +1,11 @@
 import csv
 import calendar
-import datetime
+from datetime import date
 
 lines = []
 lines_numb = 0
 email_numb = 0
-email_list = []
+all_emails = {}
 filename = 'C:\\Users\\Admin\\Desktop\\versada\\birthdays.csv'
 # filename = "C:\\Users\\Admin\\Desktop\\versada\\birthdays2.csv"
 with open(filename, newline='') as csvfile:
@@ -21,12 +21,41 @@ with open(filename, newline='') as csvfile:
                     dayok = calendar.monthrange(int(bday[0]),int(bday[1]))[1]
                     if (int(bday[2]) <= dayok) and (int(bday[1]) > 0) and (int(bday[1]) < 13): 
                         email_numb +=1
-                        email_list.append(line[1])
+                        all_emails.update({line[1]:[line[0], [int(bday[0]), int(bday[1]), int(bday[2])]]})
                     
         else:
             print("File is empty")
     else:
         print("Can't open file")
 
+def send_email(email, name, bname, bday, delta):
+    if (bday[1] < 10) or (bday[2] < 10):
+        birthday = f"{bday[0]}-0{bday[1]}-0{bday[2]}"
+    else:
+        birthday = f"{bday[0]}-{bday[1]}-{bday[2]}"
+    print(f"Sedning email to {email}")
+    print(f"Subject: Birthday Reminder: {bname}'s birthday on {birthday}")
+    print("Body:")
+    print(f"Hi {name}")
+    print(f"This is a reminder that {bname} will be celebrating their birthday on {birthday}.")
+    print(f"There are {delta} days left to get a present!")
+
+send_no = []
+send_yes = []
 if lines_numb == email_numb:
-    print(len(email_list), email_list)
+    for k, v in all_emails.items():
+        dateinfo = v[1]
+        today = date.today()
+        year = date.today().year
+        dbday = date(year, *dateinfo[1:])
+        delta = dbday - today
+        delta = delta.days
+        if delta == 7:
+            send_no.append([*v, delta])
+        else:
+            send_yes.append([k, *v[:-1]])
+
+if send_no and send_yes:    
+    for y in send_yes:
+        for n in send_no:
+            send_email(y[0], y[1], n[0], n[1], n[2])
